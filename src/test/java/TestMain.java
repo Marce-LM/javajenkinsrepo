cd8f0d02280151b68a73702661853ec74a77f3e5
Marce-LM <marcelopezmiguel@outlook.com>
2021-12-01 18:48:03 -0600

    first commit

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+    @Test
+    public void testInputIsEven(){
+        Assertions.assertTrue(Main.checkIfInputIsAnEvenNumber(22));
+    }
+}
